cb5d45d61ff97e06d02c0828b2fa7af51d7057d7
Ajay Rayasam <ajayrayasam@gmail.com>
2013-08-28 16:08:01 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Watchung::Application.config.secret_token = 'a5e6472e54ddc54faff87560152ad875d5f495fffee24867a593d6409d296305a4fc079ace5c9123c832e9778e54a1d3d975ec77761ed9cc67fd93f4c9658ad2'
